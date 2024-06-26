e5b8c08807fdcd9d0b25d34a3db81f43977d3f01
Unofficial Life <118039878+Unofficial-Life@users.noreply.github.com>
2024-06-26 19:47:22 +0000

    trying to add Readme.md fetch

--- script/repoFetch.js
+++ script/repoFetch.js
@@ -1,2 +1,21 @@
-// listrepos("ziadOUA", document.getElementById("repo-list-container")).then(reposcount => {
-// });
+function fetchReadme() {
+    var user = 'Unofficial-Life';
+    var repo = 'Unofficial-Life';
+    var readmeURL = 'https://api.github.com/repos/' + user + '/' + repo + '/readme';
+    
+    // Display loading message
+    $(".info-wrapper").html('<p>Loading README.md...</p>');
+    
+    $.get(readmeURL, function(data) {
+        var readmeContent = atob(data.content);
+        var readmeHTML = marked(readmeContent);
+        $(".info-wrapper").html('<div class="repo-readme-wrapper"><h2>' + repo + '</h2>' + readmeHTML + '</div>');
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.error("Error fetching README.md:", textStatus, errorThrown); // Log the error
+        $(".info-wrapper").html('<p>Failed to fetch README.md for ' + repo + '.</p>');
+    });
+}
+
+$(document).ready(function() {
+    fetchReadme();
+});
